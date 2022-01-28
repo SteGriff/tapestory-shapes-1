9a1064cb8e2a69b258c0c2eb3f4235426d14611f
Glitch (glitch-hello-vue) <none>
2022-01-28 21:16:45 +0000

    👶🎱 Checkpoint ./src/components/Box.vue:1433977/574 ./index.html:1433977/217 ./src/assets/tachyons.css:1433977/73972 ./src/assets/tapestory.css:1433977/129

---
+++ src/components/Box.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="['box', shapeClass, colourClass]">{{ msg }}</div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  text: String,
+  shapeClass : String,
+  shaderClass : String,
+  colourClass : String
+})
+
+</script>
