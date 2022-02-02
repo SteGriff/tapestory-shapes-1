a48ee2172c925baeef62a4267e8d7b6c3393c9bd
Glitch (glitch-hello-vue) <none>
2022-02-02 21:43:08 +0000

    😘💳 Checkpoint ./src/App.vue:1433977/485 ./public/tapestory.css:1433977/713

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,13 @@
     
     <box :text="message" shape="round" shader="gradient" :palette="0"/>
     <div class="mv4 ba br4">
-      <div class="overflow-x-scroll pa4">
-        <div v-for="p of palettes" :class="'palette' + p">
+      <div class="overflow-x-scroll pa3">
+        <div v-for="p in palettes" :class="['palette' + p, 'mh1', 'dib']">
+          <div class="dib w1 h2 bg-first"></div>
+          <div class="dib w1 h2 bg-second"></div>
+          <div class="dib w1 h2 bg-third"></div>
+          <div class="dib w1 h2 bg-fourth"></div>
+          <div class="dib w1 h2 bg-fifth"></div>
         </div>
       </div>
     </div>
@@ -19,7 +24,8 @@ import { ref } from 'vue';
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 const message = ref("Hello World");
-  
+const palettes = [0,1,3,5,6,7,8,11,12,13,14,15,16,17,18,20,22,23,26,27,28,29,34,35,37,38,40,45,47,50,55,57,60,67,69,71,73];
+
 </script>
 
 <style>
