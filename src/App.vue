0db56d0f77e3975d69e6c0ee6f47e1c0115417ea
Glitch (glitch-hello-vue) <none>
2022-02-03 17:43:33 +0000

    💕🏦 Checkpoint ./src/App.vue:1433977/5073 ./public/tapestory.css:1433977/3483

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <main class="sans-serif w-99 measure center mv4">
 <!--     <input v-model="message" /> -->
     
-    <box :text="message" shape="round" shader="gradient" :palette="0"/>
-    <div class="mv4 ba br4">
-      <div class="overflow-x-scroll pa3 ws-nw">
-        <div v-for="p in palettes" :class="['palette' + p, 'mh1', 'dib', 'ba', 'pal']">
+    <box :text="message" shape="round" shader="g135" :palette="palette"/>
+    <div class="mv4 ba br4 overflow-hidden">
+      <div class="overflow-x-scroll pv3 ph2 ws-nw">
+        <div v-for="p in palettes" :class="['palette' + p]" class="mh1 dib ba pal br2 pointer overflow-hidden">
           <div class="dib palSlice bg-first"></div>
           <div class="dib palSlice bg-second"></div>
           <div class="dib palSlice bg-third"></div>
@@ -21,10 +21,11 @@
 import box from "./components/Box.vue";
 import { ref } from 'vue';
   
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 const message = ref("Hello World");
 const palettes = [0,1,3,5,6,7,8,11,12,13,14,15,16,17,18,20,22,23,26,27,28,29,34,35,37,38,40,45,47,50,55,57,60,67,69,71,73];
+const shaders = ['1', 'g135', 'g12345', 'a','b','c','d','e']
+  
+const palette = ref(0);
 
 </script>
 
