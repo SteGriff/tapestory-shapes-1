adae5a6ea83d165b69c3e2471963e0a4119c461a
Glitch (glitch-hello-vue) <none>
2022-02-03 18:06:22 +0000

    🕴💋 Checkpoint ./public/tapestory.css:1433977/476 ./src/App.vue:1433977/513

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <main class="sans-serif w-99 measure center mv4">
 <!--     <input v-model="message" /> -->
     
-    <box :text="message" shape="round" shader="g135" :palette="palette"/>
+    <box :text="message" shape="round" :shader="shader" :palette="palette"/>
     
     <!-- Palette Selector -->
     <div class="mv4 ba br4 overflow-hidden">
@@ -31,6 +31,17 @@
       </div>
     </div>
     
+    <!-- Forground Selector -->
+    <div class="mv4 ba br4 overflow-hidden">
+      <div class="overflow-x-scroll pv3 ph2 ws-nw">
+        <div v-for="c in colors" 
+             :class="['shader' + s, 'palette' + palette, c]" 
+             class="mh1 dib sqr32 br2 pointer"
+             @click="setShader(s)">
+        </div>
+      </div>
+    </div>
+    
     <ul>
       
       <li>Palette {{palette}}</li>
@@ -42,16 +53,16 @@
 </template>
 
 <script setup>
-import box from "./components/Box.vue";
-import { ref } from 'vue';
-  
-const message = ref("Hello World");
-const palettes = [0,1,3,5,6,7,8,11,12,13,14,15,16,17,18,20,22,23,26,27,28,29,34,35,37,38,40,45,47,50,55,57,60,67,69,71,73];
-const shaders = ['1', '2', 'g13', '3', '4', 'g35', '5', 'g135', 'a','b','c','d','e']
-  
-const palette = ref(0);
-const shader = ref('g135');
-const foreground = ref('white');
+  import box from "./components/Box.vue";
+  import { ref } from 'vue';
+
+  const message = ref("Hello World");
+  const palettes = [0,1,3,5,6,7,8,11,12,13,14,15,16,17,18,20,22,23,26,27,28,29,34,35,37,38,40,45,47,50,55,57,60,67,69,71,73];
+  const shaders = ['1', '2', '3', '4', '5', 'g13', 'g35', 'g135', 'a','b','c','d','e'];
+const colours = ['black', 'white', ]
+  const palette = ref(0);
+  const shader = ref('g135');
+  const foreground = ref('white');
   
   const setPalette = (p) => {
     console.log("setPalette", p);
